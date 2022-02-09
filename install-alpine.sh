9b099db8dfe4c45fa949f6a7d457d17a9bcf4f8d
Mohamad Supangat <hantamkoding@gmail.com>
2022-02-09 15:58:31 +0700

    ta lib new configure config

--- install-alpine.sh
+++ install-alpine.sh
@@ -13,6 +13,8 @@ sudo apk add --no-cache alpine-sdk \
 cd lib/
 tar -xvzf ta-lib-0.4.0-src.tar.gz && \
   cd ta-lib/ && \
+  wget 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD' -O config.sub && \
+  wget 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD' -O config.guess
   ./configure --prefix=/usr && \
   make && \
   sudo make install
